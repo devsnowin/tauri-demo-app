1c2f789520b0db4d3bd0efb938d6b18dac35ac5b
snowin <snowin.developer@gmail.com>
2023-10-27 18:55:44 +0530

    fixed: windows sqlite not support error

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 mod commands;
 #[path = "./lib/mod.rs"]
